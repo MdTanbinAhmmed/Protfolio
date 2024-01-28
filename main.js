bfcd8d11df8c537509c6e676412607eb35fcc148
Tanbin Ahmmed <rafimdtanbinahmmed@gmail.com>
2024-01-28 16:56:23 +0600

    portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+  const typed = new Typed('.typed', {
+    strings: ['Front-End Web Developer', 'Engineer', 'Programmer'],
+    typeSpeed: 80,
+    backspeed: 150,
+    backDelay: 1500,
+    loop: true,
+    loopCount: 1100000,
+    cursorChar: '.'
+  });
+
+
+ 
